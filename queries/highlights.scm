bf5eaac87bae66b6d42674545d31efb793620ff1
lpnh <paniguel.lpnh@gmail.com>
2025-07-22 16:32:36 -0300

    improve highlights

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,46 +1,14 @@
-[
-  "fn"
-  "let"
-  "if"
-  "else"
-  "while"
-] @keyword
-
+(identifier) @variable
 (primitive_type) @type.builtin
-(unit_type) @type.builtin
-
-(function_item
-  name: (identifier) @function)
 
 (call_expression
   function: (identifier) @function)
 
-(parameter
-  name: (identifier) @variable.parameter)
-
-(let_statement
-  name: (identifier) @variable)
-
-(assignment_statement
-  name: (identifier) @variable)
-
-(identifier) @variable
-
-(integer_literal) @number
-(boolean_literal) @boolean
-(unit_literal) @constant.builtin
+(function_item
+  name: (identifier) @function)
 
 (line_comment) @comment
 (block_comment) @comment
-
-(binary_expression
-  operator: _ @operator)
-
-[
-  "="
-  "->"
-  ":"
-] @operator
 
 [
   "("
@@ -53,3 +21,17 @@
   ","
   ";"
 ] @punctuation.delimiter
+
+(parameter
+  (identifier) @variable.parameter)
+
+[
+  "fn"
+  "let"
+  "if"
+  "else"
+  "while"
+] @keyword
+
+(boolean_literal) @boolean
+(integer_literal) @number
